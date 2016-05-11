8320a477d7d40ed41acda7abf102f6a5df95c427
Michael Staehling <Michael.staehling@gmail.com>
2016-05-10 23:37:57 -0400

    2 unordered lists of addresses pulled from google api seperated based on whether there's a zipcode or not

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,18 +2,15 @@
   'use strict';
 
   angular.module('boldtech')
-    .controller('MainController', function($http, $q, $scope) {
-      var vm = this;
-      var defer = $q.defer();
+    .controller('MainController', function($http, $scope) {
 
       $http({
           method: 'GET',
           url: 'https://maps.googleapis.com/maps/api/geocode/json?address=orlando'
         })
         .then(function(response) {
-          vm.location = response.data.results;
+          $scope.location = response.data.results;
           console.log(response.data.results);
-          defer.resolve;
         });
 
     });
